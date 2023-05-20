a5e6e213083893928cf808aaacb007fca7e5efd2
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-05-20 00:53:29 -0300

    :sparkles: feat: Change About page

--- src/data/app-config.js
+++ src/data/app-config.js
@@ -2,7 +2,8 @@ const appConfig = {
   application: {
     name: 'Country Hook',
     version: '0.0.1',
-    description: '.',
+    description:
+      'This project is a React application utilizing a custom hook named `useCountry` to search for specific country details from the service available at https://studies.cs.helsinki.fi/restcountries/. If the country is found, the application displays its details. If the country is not found, a corresponding message is shown instead. The `useCountry` hook relies on the API endpoint name to fetch the details of a country within a `useEffect` hook. The second parameter array plays a vital role in controlling the execution of the effect function. This exercise forms a part of the Fullstack Open course.',
   },
   footer: {
     createdBy: 'Josenaldo Matos',
